a6fd1e548e2b1ce1703a9b4fc71149e2321a13db
Mike Duncan <mrduncanm@gmail.com>
2023-01-28 14:34:06 -0800

    Small fixes.

--- ssd1315_img.h
+++ ssd1315_img.h
@@ -22,7 +22,7 @@ void ssd1315_img_init(SSD1315_IMG *img);
 
 void ssd1315_img_array_init(SSD1315_IMG *imgs, uint8_t *buffer, uint8_t size, uint8_t colsX, uint8_t pagesY);
 
-#define SSD1315_IMG_ARRAY(var_, size_, colsX_, pagesY_) uint8_t var_ ## Buf[size_ * (colsX_ * pagesY_ + 1)]; SSD1315_IMG var_ ## [size_]; ssd1315_img_array_init(&var_, var_ ## Buf, size_, colsX_, pagesY_)
+#define SSD1315_IMG_ARRAY(var_, size_, colsX_, pagesY_) uint8_t var_ ## Buf[size_ * (colsX_ * pagesY_ + 1)]; SSD1315_IMG var_ ## [size_]; ssd1315_img_array_init((SSD1315_IMG *)&var_, var_ ## Buf, size_, colsX_, pagesY_)
 
 void ssd1315_img_clear(SSD1315_IMG *img);
 
